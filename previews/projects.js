20491c4203f8ebdf0c72dcf8fcc829c4584bb50a
Ashish Bhattarai <vattevaii@gmail.com>
2021-11-18 22:36:22 +0545

    Added page for previews

---
+++ previews/projects.js
@@ -0,0 +1,38 @@
+export default{"projects": [
+    {
+        "name": "1st Template",
+        "desination": "Front-End",
+        "link": "href:://facebook.com",
+        "img":"./1.png"
+    },
+    {
+        "name": "2nd Template",
+        "desination": "Front-End",
+        "link": "href:://facebook.com","img":"./1.png"
+    },
+    {
+        "name": "3rd Template",
+        "desination": "Front-End",
+        "link": "href:://facebook.com","img":"./1.png"
+    },
+    {
+        "name": "4th Template",
+        "desination": "Front-End",
+        "link": "href:://facebook.com","img":"./1.png"
+    },
+    {
+        "name": "5th Template",
+        "desination": "Front-End",
+        "link": "href:://facebook.com","img":"./1.png"
+    },
+    {
+        "name": "6th Template",
+        "desination": "Front-End",
+        "link": "href:://facebook.com","img":"./1.png"
+    },
+    {
+        "name": "7th Template",
+        "desination": "Front-End",
+        "link": "href:://facebook.com","img":"./1.png"
+    }
+]}
---
+++ previews/style.js
@@ -0,0 +1,44 @@
+import projects from "./projects.js";
+console.log(projects);
+const overlay = document.querySelector('.overlay');
+const overlayImage = document.querySelector('.overlay__inner img');
+const overlayLink = document.querySelector('.overlay__inner a');
+
+function open(e) {
+    overlay.classList.add('open');
+    const src = e.currentTarget.querySelector('img').src;
+    const href = e.currentTarget.querySelector('a') ? e.currentTarget.querySelector('a').href : '#';
+    overlayImage.src = src;
+    overlayLink.href = href;
+}
+
+function close() {
+    overlay.classList.remove('open');
+}
+
+const render = function (projects) {
+    projects["projects"].forEach(p => {
+        const project = document.createElement("div");
+        project.classList.add("project");
+        project.innerHTML = `<img class="project__image"
+        src="${p.img}" />
+    <div class="grid__overlay">
+        <p>${p.name}</p>
+        <h3 class="grid__title">${p.desination}</h3>
+    </div>
+    <div class="grid__overlay animatable">
+        <button class="viewbutton">view more</button>
+    </div>`;
+        overlay.insertAdjacentElement("beforebegin", project);
+    })
+}
+
+window.onload = function () {
+    render(projects);
+    const buttons = document.querySelectorAll('.project');
+    buttons.forEach(button => {
+        button.addEventListener('click', open);
+    });
+    overlay.addEventListener('click', close);
+}
+
