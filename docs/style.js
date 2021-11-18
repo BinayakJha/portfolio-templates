a6cd9744fa3e70d733e8cb7d1f6567b2f0c0b36e
Restyled.io <commits@restyled.io>
2021-11-18 21:07:55 +0000

    Restyled by clang-format

--- docs/style.js
+++ docs/style.js
@@ -5,22 +5,23 @@ const overlayImage = document.querySelector('.overlay__inner img');
 const overlayLink = document.querySelector('.overlay__inner a');
 
 function open(e) {
-    overlay.classList.add('open');
-    const src = e.currentTarget.querySelector('img').src;
-    const href = e.currentTarget.querySelector('a') ? e.currentTarget.querySelector('a').href : '#';
-    overlayImage.src = src;
-    overlayLink.href = href;
+  overlay.classList.add('open');
+  const src = e.currentTarget.querySelector('img').src;
+  const href = e.currentTarget.querySelector('a')
+                   ? e.currentTarget.querySelector('a').href
+                   : '#';
+  overlayImage.src = src;
+  overlayLink.href = href;
 }
 
-function close() {
-    overlay.classList.remove('open');
-}
+function close() { overlay.classList.remove('open'); }
 
-const render = function (projects) {
-    projects["projects"].forEach(p => {
-        const project = document.createElement("div");
-        project.classList.add("project");
-        project.innerHTML = `<img class="project__image"
+const render =
+    function(projects) {
+  projects["projects"].forEach(p => {
+    const project = document.createElement("div");
+    project.classList.add("project");
+    project.innerHTML = `<img class="project__image"
         src="./docs/${p.img}" />
     <div class="grid__overlay">
         <p>${p.name}</p>
@@ -30,16 +31,13 @@ const render = function (projects) {
     <div class="grid__overlay animatable">
         <button class="viewbutton">view more</button>
     </div>`;
-        overlay.insertAdjacentElement("beforebegin", project);
-    })
+    overlay.insertAdjacentElement("beforebegin", project);
+  })
 }
 
-window.onload = function () {
-    render(projects);
-    const buttons = document.querySelectorAll('.project');
-    buttons.forEach(button => {
-        button.addEventListener('click', open);
-    });
-    overlay.addEventListener('click', close);
+    window.onload = function() {
+  render(projects);
+  const buttons = document.querySelectorAll('.project');
+  buttons.forEach(button => { button.addEventListener('click', open); });
+  overlay.addEventListener('click', close);
 }
-
